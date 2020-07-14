0c7889e04d5307cb4e33d73a503cdc26abf66909
Phil Wang <lucidrains@gmail.com>
2020-07-14 12:24:49 -0700

    update setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'scattering-transform',
+  packages = find_packages(),
+  version = '0.0.1',
+  license='MIT',
+  description = 'Scattering Transform module from the paper Scattering Compositional Learner',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/scattering-compositional-learner',
+  keywords = ['artificial intelligence', 'deep learning', 'reasoning'],
+  install_requires=[
+      'torch'
+  ],
+  classifiers=[
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'Topic :: Scientific/Engineering :: Artificial Intelligence',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 3.6',
+  ],
+)
